d4fb3e496d9ef8aad15af89f6fe21718d7d7473f
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2022-07-06 09:36:17 -0300

    source img

--- 100-days-Web-Dev/02-summary/js/script.js
+++ 100-days-Web-Dev/02-summary/js/script.js
@@ -1,10 +1,8 @@
 function printFunc() {
-  console.log("Hello f* world!");
+  for (let count = 0; count <= 10; count++) {
+    console.log(count);
+    console.log("Hello f* world!");
+  }
 }
 
 printFunc();
-
-for (let count = 0; count <= 10; count++) {
-    printFunc();
-    console.log(count);
-}
