670b52ac65f2e36a942f7dceaba2ea9ad13a956b
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-01-03 11:34:28 -0300

    Third commit day of the year

---
+++ js/Andre_Iacono/day01/main.js
@@ -0,0 +1,32 @@
+alert("Hello world!");
+
+console.log("Javascript funcionando!");
+
+document.write("Hello World.");
+
+/*
+Variáveis
+    - Regras para nomear variáveis
+    - var, let e const
+            let vc pode alterar
+        const: vc não pode alterar
+
+    Tipos: 
+        Primitivas
+            String
+            Number
+            Boolean
+            Undefined
+            Null
+            Object
+*/
+
+let completeName = "Shaquil Barrett";
+let firstName = "Jake";
+let lastName = "Camarda";
+let itemName = "Pen";
+let number = 3;
+let itemAvailable = true;
+let itemColor = "Red";
+
+console.log(typeof completeName);
