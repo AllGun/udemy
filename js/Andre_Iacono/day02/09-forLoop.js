ad4ff5b217c9fdeaaf82fbe5e9e17c10713b6dee
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-01-08 21:03:15 -0300

    While loop

---
+++ js/Andre_Iacono/day02/09-forLoop.js
@@ -0,0 +1,14 @@
+//For Looping
+
+function imprimirNumeros(num) {
+  let i;
+  for (i = 1; i <= num; i++) {
+    console.log(`Numero ${i}`);
+  }
+  console.log();
+  console.log(i);
+  console.log();
+}
+
+imprimirNumeros(10);
+imprimirNumeros(5);
