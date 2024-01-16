4d230e1eda86acf41ef96a8cfea652a5a3bf2d34
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-01-16 11:01:53 -0300

    Commit - Dia 16

--- js/Andre_Iacono/projects/Tip/Antes/index.js
+++ js/Andre_Iacono/projects/Tip/Antes/index.js
@@ -2,7 +2,26 @@ function update() {
   let bill = Number(document.getElementById("yourBill").value);
   let tipPercent = document.getElementById("tipInput").value;
   let split = document.getElementById("splitInput").value;
+
   console.log({ bill, tipPercent, split });
-  let teste = 11;
-  console.log({ teste });
+
+  let tipValue = (bill * tipPercent) / 100;
+  let billTotal = bill + tipValue;
+  let billEach = billTotal / split;
+
+  console.log({ tipValue, billTotal, billEach });
+
+  document.getElementById("tipPercent").innerHTML = `${tipPercent} %`;
+  document.getElementById("tipValue").innerHTML = `R$ ${tipValue.toFixed(2)}`;
+  document.getElementById("totalWithTip").innerHTML = `R$ ${billTotal.toFixed(
+    2
+  )}`;
+
+  if (split == 1) {
+    document.getElementById("splitValue").innerHTML = `${split} pessoa`;
+  } else {
+    document.getElementById("splitValue").innerHTML = `${split} pessoas`;
+  }
+
+  document.getElementById("billEach").innerHTML = `R$ ${billEach.toFixed(2)}`;
 }
