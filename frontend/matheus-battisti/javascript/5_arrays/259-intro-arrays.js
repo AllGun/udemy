28f30cb90b0ecee7069ae02d818029d8733c90ed
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-02-03 16:22:50 -0300

    arrays e objects

--- frontend/matheus-battisti/javascript/5_arrays/259-intro-arrays.js
+++ frontend/matheus-battisti/javascript/5_arrays/259-intro-arrays.js
@@ -1,1 +1,12 @@
 let numeros = [1, 2, 3, 4, 5];
+
+let userName = "Alfredo Gutemberg";
+
+console.log(userName.length);
+
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName[0].toLowerCase());
+
+console.log(typeof userName.toLocaleUpperCase);
+console.log(typeof userName);
