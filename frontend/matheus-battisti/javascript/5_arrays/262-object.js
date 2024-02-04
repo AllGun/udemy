de88beabbbf233d41b348feecdcf9ef2a9bffbfd
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-02-04 20:05:39 -0300

    obj literals

---
+++ frontend/matheus-battisti/javascript/5_arrays/262-object.js
@@ -0,0 +1,11 @@
+// Object Literals
+const person = {
+  name: "Matheus",
+  age: 32,
+  job: "Developer",
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.name.length);
+console.log(typeof person);
