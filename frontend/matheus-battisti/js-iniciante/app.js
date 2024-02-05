0be9b3c6fb8c85b13d3537d16f2bb6a08343425e
Alfredo Gutemberg <alfredo.gutemberg@gmail.com>
2024-02-04 22:12:24 -0300

    manipulando forms

---
+++ frontend/matheus-battisti/js-iniciante/app.js
@@ -0,0 +1,6 @@
+//let campoNome = (document.getElementById("nome").value = "Matheus Battisti");
+//let campoCheckbox = (document.getElementById("meuCheckbox").checked = true);
+
+let campoNome = document.getElementById();
+//lê o valor atual do campo
+//let nomeAtual = campoNome.value;
